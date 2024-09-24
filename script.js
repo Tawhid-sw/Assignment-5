4c5d2752ee99e8ae83bd4453ce1c2f049ec8346a
Tawhid-sw <tawhidsarkar684@gmail.com>
2024-09-24 23:56:02 +0600

    make the funtion for addding balance and cutting from acc balance

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 // alert("Hello");
 
 const Main = document.getElementById("main");
+const AccountBalance = document.getElementById("balance");
 const DonateBtn = document.querySelectorAll(".Donate-now");
 const DonateAmount = document.querySelectorAll(".DonateAmount");
+const TotalDonate = document.querySelectorAll(".Total_Donate");
+let Balance = 7000;
 const getIndex = DonateBtn.forEach((btn, index) => {
   btn.onclick = () => {
-    return index;
+    let NewBalance;
+    const Amount = parseInt(DonateAmount[index].value);
+    if (isNaN(Amount)) {
+      alert("Wrond Input");
+    } else {
+      NewBalance = parseInt(Balance) - Amount;
+      Balance = NewBalance;
+      AccountBalance.innerHTML = `<img src="./assets/coin.png" alt="balance" /> ${Balance} BDT`;
+      TotalDonate[
+        index
+      ].innerHTML = `<img src="./assets/coin.png" alt="coin" /> ${Amount} BDT`;
+      DonateAmount[index].value = "";
+      console.log(Amount);
+    }
   };
 });
